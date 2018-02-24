88b8490633611f6fefc714a76b22b65c44a5ce07
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-24 18:14:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 binding.pry
+  holiday_hash.each do |season, holiday|
+    puts "#{season}:"
+    puts holiday
 end
 
 def all_holidays_with_bbq(holiday_hash)
