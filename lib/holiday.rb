fcd0bfbc20ef331dadef2ade888c1ebc85318058
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-24 18:04:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,6 +53,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   all_winter_holiday_supplies = []
   holiday_hash[:winter].each do |key, value|
+    all_winter_holiday_supplies << value
   binding.pry
 
   end
