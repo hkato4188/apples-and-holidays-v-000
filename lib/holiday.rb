2aa3e51145b3dccba8db695627ab4964ee39da4e
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-24 19:47:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect do |season, holidays|
+binding.pry
     holidays.collect do |holiday, supplies|
       if supplies.include? {|item| item == "BBQ"}
       end
