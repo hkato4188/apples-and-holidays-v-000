95769922ab9f303f5e09ff40396de261a367e14b
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-24 18:02:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,6 +54,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |key, value|
   binding.pry
 end
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
