e648f832a12111cfa4a4f501c04d93dc1f642a31
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-24 19:26:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize!}:"
     holidays.each do |holiday, supplies|
       binding.pry
-    puts "  #{holiday.to_s.sub("_"," ").split.map(&:capitalize).join(' ')!}: #{supplies.join(", ")}"
+    puts "  #{holiday.to_s.sub("_"," ").split.map(&:capitalize).join(' ')}: #{supplies.join(", ")}"
 
 
     end
